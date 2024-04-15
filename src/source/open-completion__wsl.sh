a0d410c3a2df765e5522bf3cd2ce4c8b16019852
Yuichiro Luke Smith <contact@yu-smith.com>
2024-04-15 23:40:41 +0900

    delete: 🗑️ removed personal bin/sources

--- src/source/open-completion__wsl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# define completion for `open` and `xdg-open`
-function _open_completion {
-  _files
-  return 1;
-}
-
-compdef _open_completion open
-compdef _open_completion xdg-open
