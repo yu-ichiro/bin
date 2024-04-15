5926ff59202fcc1f762037a62482dd87fea95b69
Yuichiro Luke Smith <contact@yu-smith.com>
2024-04-16 02:20:06 +0900

    feat: ✨ added process related functions

---
+++ src/source/process_funcs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# define functions to display informations about a process
+function ppid() {
+    pid=${1:-$$}
+    ps -p -o ppid= | sed -E 's/\s//g'
+}
+
+function pcomm() {
+    pid ${1:-$$}
+    ps -p $pid -o comm=
+}
+
+function rppid() {
+    pid=${1:-$$}
+    while true; do
+        echo $pid $(pcomm $pid)
+        pid=$(ppid $pid 2>/dev/null)
+        [ $pid != "" -a $pid != 0 ] || break
+    done
+}
+
