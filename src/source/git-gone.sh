eacbaf1b2bc79eb2f8a32ab7c8abfa1125be0668
Yuichiro Luke Smith <contact@yu-smith.com>
2024-04-28 22:34:10 +0900

    chore: 🔧 make installation of git-gone independent of ghr

--- src/source/git-gone.sh
+++ src/source/git-gone.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 # clone/pull git-gone
 
-defer << EOF
-    source <(ghr shell bash)
-    ghr clone --cd github.com:eed3si9n/git-gone
-    git pull
+export GITGONE_URL=https://github.com/eed3si9n/git-gone
+export GITGONE_DIR=$VCS_HOME/${GITGONE_URL#https://}
+
+cmd=(defer)
+# cmd=($SHELL)
+"$cmd[@]" <<EOF
+    if [ ! -d "\$GITGONE_DIR" ]; then
+	echo -n "Cloning git-gone..."
+	>/dev/null 2>&1 git clone \$GITGONE_URL \$GITGONE_DIR
+	echo "done"
+    fi
+    cd \$GITGONE_DIR
+    echo -n "Updating git-gone..."
+    >/dev/null 2>&1 git pull
+    echo "done"
     ln -sf \$PWD/git-gone \$MYBIN_PATH/bin/git-gone
+    ls \$MYBIN_PATH/bin
 EOF
+
+unset -v GITGONE_URL
+unset -v GITGONE_DIR
